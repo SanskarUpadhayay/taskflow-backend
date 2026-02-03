f0f600d080559f8eabbbb1a652504f85c1743c23
Sanskar Upadhyay <sanskar192000@gmail.com>
2026-02-04 02:06:43 +0530

    feat: Initialize Express API with Typescript and health endpoint

---
+++ src/index.ts
@@ -0,0 +1,22 @@
+import express, {Request, Response} from 'express';
+import dotenv from 'dotenv';
+
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+//Middleware
+app.use(express.json());
+
+//Health Check Route
+app.get('/health',(req: Request,res: Response) => {
+    res.status(200).json({
+        "status": "OK"
+    });
+});
+
+app.listen(PORT,() => {
+    console.log(`Server running on PORT ${PORT}`);
+});
